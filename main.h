c7482f7a7e7bce001cf365753d18f0307a93d614
nornormal <niyramagdy@gmail.com>
2023-03-28 07:12:13 -0700

    printf

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+
+int _printf(const char *format, ...);
+
+#endif
