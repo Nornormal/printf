3409ce4f6a6ec7dea7a37c770858df0c5a63b99b
nornormal <niyramagdy@gmail.com>
2023-03-28 19:59:29 -0700

    printf

--- main.h
+++ main.h
@@ -9,6 +9,11 @@
 
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
 
 /**
  * struct format_s - A new type defining a format struct.
@@ -18,11 +23,22 @@
 typedef struct format_s
 {
 	char fcase;
-	int (*fun)(va_list, char[], int, int, int ,int);
+	int (*fun)(va_list, char[], int, int, int, int);
 } format_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *format, int *i,
-va_list args, char buffer[], int flags, int width, int precision, int size);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+int handle_write_char(char c, char buffer[], int flags, int width,
+int precision, int size);
+int write_unsgnd(int is_negative, int ind, char buffer[],
+int flags, int width, int precision, int size);
+int is_printable(char);
+long int convert_size_number(long int num, int size);
+
 
 #endif
