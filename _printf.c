9d33c0f0ccbb7f5af2b6c01b63b7099352c057a2
saraedrees <sara.mostafa_m@yahoo.com>
2023-03-28 19:57:17 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,48 +1,104 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 /**
  * case_c - function prints character.
  * @args: input arguments.
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
  *
  * Return: number of characters printed
  */
-int case_c(va_list args)
+int case_c(va_list args, char buffer[],
+		int flags, int width, int precision, int size)
 {
 	char c = va_arg(args, int);
 
-	putchar(c);
-	return (1);
+	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 
 /**
  * case_s - function prints sting.
  * @args: input agruments.
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
  *
  * Return: number of characters printed.
  */
-int case_s(va_list args)
+int case_s(va_list args, char buffer[],
+		int flags, int width, int precision, int size)
 {
 	char *str = va_arg(args, char*);
-	int counter = 0;
+	int length = 0, i;
 
-	for (; *str; str++, counter++)
-		putchar(*str);
-	return (counter);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	if (str == NULL)
+	{
+		str = "(null)";
+		if (precision >= 6)
+			str = "      ";
+	}
+
+	while (str[length] != '\0')
+		length++;
+
+	if (precision >= 0 && precision < length)
+		length = precision;
+
+	if (width > length)
+	{
+		if (flags & F_MINUS)
+		{
+			write(1, &str[0], length);
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			write(1, &str[0], length);
+			return (width);
+		}
+	}
+
+	return (write(1, str, length));
 }
 
 /**
  * case_mod - function prints modulus(%).
  * @args: input arguments.
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
  *
  * Return: number of characters printed.
  */
-int case_mod(va_list __attribute__((unused)) args)
+int case_mod(va_list args, char buffer[],
+		int flags, int width, int precision, int size)
 {
-	putchar('%');
-	return (1);
+	UNUSED(args);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
 }
+
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - function that produces output according to a format.
@@ -54,30 +110,58 @@ int case_mod(va_list __attribute__((unused)) args)
  */
 int _printf(const char *format, ...)
 {
-	int i, counter;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list args;
-	format_t formats[] = {
-		{'c', case_c}, {'s', case_s}, {'%', case_mod}, {0, NULL}
-	};
+	char buffer[BUFF_SIZE];
 
-	if (!format)
+	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
-	for (; *format; format++)
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (*format != '%')
+		if (format[i] != '%')
 		{
-			putchar(*format);
-			counter++;
-			continue;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
-		format++;
-		for (i = 0; formats[i].fcase; i++)
-			if (formats[i].fcase == *format)
-				counter += formats[i].fun(args);
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, args);
+			precision = get_precision(format, &i, args);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, args, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
 	}
 
+	print_buffer(buffer, &buff_ind);
+
 	va_end(args);
-	return (counter);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
